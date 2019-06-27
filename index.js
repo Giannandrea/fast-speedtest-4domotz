8d4f9c4d0983208e3a28496eb79604b90a078b08
mauro giannandrea <mgiannandrea@domotz.com>
2019-06-27 17:09:21 +0200

    first commit for fast

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+var apis = require("./lib/api.js");
+var Api = apis.Api
+
+var options = {
+    token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
+    verbose: true, // default: false
+    timeout: 10000, // default: 5000
+    https: true, // default: true
+    bufferSize: 8, // default: 8
+}
+var speedtest = new Api(options);
+
+speedtest.getSpeed().then(s => {
+    console.log(`Final test result: `)
+    console.log(`\tDownload Speed: ${s} Mbps`);
+}).catch(e => {
+    console.error(e.message);
+});
