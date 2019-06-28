1b5574b1e727cb18878da6c10ed21e2128292744
mauro giannandrea <mgiannandrea@domotz.com>
2019-06-28 14:48:03 +0200

    make it compatible with node 0.10.37

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-
+Promise = require('bluebird')
 var apis = require("./lib/api.js");
 var Api = apis.Api
 
@@ -7,14 +7,14 @@ var options = {
     token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
     verbose: true, // default: false
     timeout: 10000, // default: 5000
-    https: true, // default: true
+    https: false, // default: true
     bufferSize: 8, // default: 8
 }
 var speedtest = new Api(options);
 
-speedtest.start().then(s => {
-    console.log(`Final test result: `)
-    console.log(`\tDownload Speed: ${s} Mbps`);
-}).catch(e => {
+speedtest.start().then(function(s) {
+    console.log("Final test result: ")
+    console.log("\tDownload Speed: "+s+" Mbps");
+}).catch(function(e) {
     console.error(e.message);
 });
